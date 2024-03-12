7e14c30f32cfa27df951210e4b9b0b12dd779d44
AntoineP2 <antoine.vinsonneau34@gmail.com>
2024-03-12 22:35:23 +0100

    refacto form desktop

--- frontend/app/components/Forms.tsx
+++ frontend/app/components/Forms.tsx
@@ -67,80 +67,82 @@ const Forms = () => {
           </div>
 
           {/* NOM, PRENOM et EMAIL*/}
-          <div className="flex gap-5 md:flex-row flex-col md:flex-wrap md:justify-center justify-start bg-base-200 p-4 rounded-lg mb-6 md:mb-11 shadow shadow-primary">
-            <div className="flex flex-col gap-2">
-              <label className="input input-bordered input-sm input-primary bg-base-300 flex items-center gap-2">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 16 16"
-                  fill="currentColor"
-                  className="w-4 h-4 opacity-70 text-accent"
-                >
-                  <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.215 0c-.22.578.254 1.139.872 1.139h9.47Z" />
-                </svg>
-                <input
-                  type="text"
-                  className="grow"
-                  placeholder="Prénom"
-                  disabled={submitLoading}
-                  {...register("firstName", {
-                    required: true,
-                    pattern: /^[A-Za-z]+$/,
-                    maxLength: 20,
-                  })}
-                />
-              </label>
-              {errors.firstName?.type === "pattern" && (
-                <span className="text-error text-xs">
-                  Le prénom doit être composé de lettre uniquement
-                </span>
-              )}
-              {errors.firstName?.type === "required" && (
-                <span className="text-error text-xs">
-                  Le prénom est obligatoire
-                </span>
-              )}
-              {errors.firstName?.type === "maxLength" && (
-                <span className="text-error text-xs">
-                  Le prénomn est trop long
-                </span>
-              )}
-            </div>
-            <div className="flex flex-col gap-2">
-              <label className="input input-bordered input-sm input-primary bg-base-300 flex items-center gap-2">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 16 16"
-                  fill="currentColor"
-                  className="w-4 h-4 opacity-70 text-accent"
-                >
-                  <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.215 0c-.22.578.254 1.139.872 1.139h9.47Z" />
-                </svg>
-                <input
-                  type="text"
-                  className="grow"
-                  placeholder="Nom"
-                  disabled={submitLoading}
-                  {...register("lastName", {
-                    required: true,
-                    pattern: /^[A-Za-z]+$/,
-                    maxLength: 20,
-                  })}
-                />
-              </label>
-              {errors.lastName?.type === "pattern" && (
-                <span className="text-error text-xs">
-                  Le nom doit être composé de lettre uniquement
-                </span>
-              )}
-              {errors.lastName?.type === "required" && (
-                <span className="text-error text-xs">
-                  Le nom est obligatoire
-                </span>
-              )}
-              {errors.lastName?.type === "maxLength" && (
-                <span className="text-error text-xs">Le nom est trop long</span>
-              )}
+          <div className="flex gap-5 flex-col justify-start bg-base-200 p-4 rounded-lg mb-6 md:mb-11 shadow shadow-primary">
+            <div className="flex md:flex-wrap md:flew-row   flex-col gap-5">
+              <div className="flex flex-col gap-2 w-full flex-1">
+                <label className="input input-bordered input-sm input-primary bg-base-300 flex items-center gap-2">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 16 16"
+                    fill="currentColor"
+                    className="w-4 h-4 opacity-70 text-accent"
+                  >
+                    <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.215 0c-.22.578.254 1.139.872 1.139h9.47Z" />
+                  </svg>
+                  <input
+                    type="text"
+                    className="grow"
+                    placeholder="Prénom"
+                    disabled={submitLoading}
+                    {...register("firstName", {
+                      required: true,
+                      pattern: /^[A-Za-z]+$/,
+                      maxLength: 20,
+                    })}
+                  />
+                </label>
+                {errors.firstName?.type === "pattern" && (
+                  <span className="text-error text-xs">
+                    Le prénom doit être composé de lettre uniquement
+                  </span>
+                )}
+                {errors.firstName?.type === "required" && (
+                  <span className="text-error text-xs">
+                    Le prénom est obligatoire
+                  </span>
+                )}
+                {errors.firstName?.type === "maxLength" && (
+                  <span className="text-error text-xs">
+                    Le prénomn est trop long
+                  </span>
+                )}
+              </div>
+              <div className="flex flex-col gap-2 w-full flex-1">
+                <label className="input input-bordered input-sm input-primary bg-base-300 flex items-center gap-2">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 16 16"
+                    fill="currentColor"
+                    className="w-4 h-4 opacity-70 text-accent"
+                  >
+                    <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.215 0c-.22.578.254 1.139.872 1.139h9.47Z" />
+                  </svg>
+                  <input
+                    type="text"
+                    className="grow"
+                    placeholder="Nom"
+                    disabled={submitLoading}
+                    {...register("lastName", {
+                      required: true,
+                      pattern: /^[A-Za-z]+$/,
+                      maxLength: 20,
+                    })}
+                  />
+                </label>
+                {errors.lastName?.type === "pattern" && (
+                  <span className="text-error text-xs">
+                    Le nom doit être composé de lettre uniquement
+                  </span>
+                )}
+                {errors.lastName?.type === "required" && (
+                  <span className="text-error text-xs">
+                    Le nom est obligatoire
+                  </span>
+                )}
+                {errors.lastName?.type === "maxLength" && (
+                  <span className="text-error text-xs">Le nom est trop long</span>
+                )}
+              </div>
             </div>
             <div className="flex-col gap-2 md:flex">
               <label className="input input-bordered input-sm input-primary bg-base-300 flex items-center gap-2">
@@ -184,10 +186,10 @@ const Forms = () => {
           </div>
 
           {/* SECTOR ET LEVEL */}
-          <div className="flex gap-5 md:flex-row flex-col justify-start bg-base-200 p-4 rounded-lg mb-6 md:mb-11 shadow shadow-primary md:flex-wrap md:justify-around">
+          <div className="flex gap-5 flex-col justify-start bg-base-200 p-4 rounded-lg mb-6 md:mb-11 shadow shadow-primary">
             <select
               disabled={submitLoading}
-              className="select select-primary w-full bg-base-300 select-sm max-w-xs"
+              className="select select-primary w-full bg-base-300 select-sm "
               {...register("sector", { required: true })}
             >
               <option disabled selected>
@@ -204,7 +206,7 @@ const Forms = () => {
 
             <select
               disabled={submitLoading}
-              className="select select-primary w-full bg-base-300 select-sm max-w-xs"
+              className="select select-primary w-full bg-base-300 select-sm"
               {...register("level", { required: true })}
             >
               <option disabled selected>
