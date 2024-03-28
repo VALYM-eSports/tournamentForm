937fd5a4ec0273d0a41e0e1964a78f83c658c0a7
unknown <antoine.vinsonneau34@gmail.Com>
2024-03-28 10:04:55 +0100

    synthaxe

--- frontend/app/decline/page.tsx
+++ frontend/app/decline/page.tsx
@@ -9,12 +9,12 @@ const declinePage = () => {
       <div className="bg-base-200 p-4 rounded-lg mb-6 md:mb-11 shadow shadow-accent">
         <p className="text-xl font-bold text-center">C&apos;est dommage...</p>
         <p className="text-xl font-bold text-center">
-          Tu rate un super tournoi...
+          Tu rates un super tournoi...
         </p>
         <p className="text-xl font-bold text-center">
           {" "}
-          Cela dit, tu peu toujours{" "}
-          <strong className="text-accent">changer d&apos;avis</strong> !
+          Cela dit, tu peux toujours{" "}
+          <strong className="text-accent">changé d&apos;avis</strong> !
         </p>
       </div>
       <div className="flex justify-center items-center mb-5">
